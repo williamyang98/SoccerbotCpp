6261fe2e30e1708c2a600c1ef2368d1e800c4744
William Yang <williamyang2017@gmail.com>
2023-09-13 14:42:26 +1000

    Replace threaded data generator with tensorflow dataset

--- scripts/training/run_training.py
+++ scripts/training/run_training.py
@@ -6,8 +6,12 @@ if __name__ == "__main__":
     parser.add_argument("--asset-path", type=str, default="../assets/", help="Path to game assets")
     parser.add_argument("--model-in", type=str, default=DEFAULT_MODEL_PATH, help="Input path for pretrained model")
     parser.add_argument("--model-out", type=str, default=DEFAULT_MODEL_PATH, help="Output path for trained model")
-    parser.add_argument("--epochs", type=int, default=100, help="Total epochs to train model")
+    parser.add_argument("--epochs", type=int, default=10, help="Total epochs to train model")
+    parser.add_argument("--steps-per-epoch", type=int, default=20, help="Total steps per epochs")
+    parser.add_argument("--batch-size", type=int, default=256, help="Batch size")
+    parser.add_argument("--total-parallel-load", type=int, default=0, help="Number of threads to spawn for generating data set")
     parser.add_argument("--device", type=str, default="GPU:0", choices=["CPU", "GPU:0"], help="Device to use for training")
+    parser.add_argument("--print-model-summary", action="store_true", help="Print model summary before training")
     args = parser.parse_args()
 
     import os
@@ -16,6 +20,12 @@ if __name__ == "__main__":
     if not os.path.exists(args.asset_path):
         print(f"[error] invalid asset path: '{args.asset_path}'")
         exit(1)
+
+    TOTAL_DATA_THREADS = args.total_parallel_load
+    if TOTAL_DATA_THREADS == 0:
+        import multiprocessing
+        TOTAL_DATA_THREADS = multiprocessing.cpu_count()
+    print(f"Using {TOTAL_DATA_THREADS} threads for data loading")
 
     pathlib.Path(os.path.dirname(args.model_out)).mkdir(parents=True, exist_ok=True)
 
@@ -42,34 +52,45 @@ if __name__ == "__main__":
     im_width, im_height = image.size
     im_channels = 3
 
-    def create_batch(batch_size):
-        X_in = np.zeros((batch_size, im_height, im_width, im_channels), dtype=np.float32)
-        Y_out = np.zeros((batch_size, 3), dtype=np.float32)
-
-        for i in range(batch_size):
-            image, bounding_box, has_ball = generator.create_sample()
-            image = image.convert("RGB")
-            image = image.resize((int(x/IMAGE_DOWNSCALE) for x in image.size))
-            
-            x_in = np.asarray(image)
-            x_in = x_in.astype(np.float32) / 255.0
-            x_in = x_in[np.newaxis,:]
-            X_in[i] = x_in
-
-            x_center, y_center, ball_width, ball_height = bounding_box
-            confidence = 0.0 if not has_ball else 1.0
-
-            Y_out[i,0] = x_center
-            Y_out[i,1] = y_center
-            Y_out[i,2] = confidence
-
-        X_in = tf.convert_to_tensor(X_in)
-        Y_out = tf.convert_to_tensor(Y_out)
-        return (X_in, Y_out)
-
-    from async_batch_generator import AsyncBatchGenerator 
-
     import tensorflow as tf
+    class ArtificialDataset(tf.data.Dataset):
+        def _generator():
+            while True:
+                y_out = np.zeros((3,), dtype=np.float32)
+
+                image, bounding_box, has_ball = generator.create_sample()
+                image = image.convert("RGB")
+                image = image.resize((int(x/IMAGE_DOWNSCALE) for x in image.size))
+                
+                x_in = np.asarray(image)
+                x_in = x_in.astype(np.float32) / 255.0
+
+                x_center, y_center, ball_width, ball_height = bounding_box
+                confidence = 0.0 if not has_ball else 1.0
+
+                x_in = tf.convert_to_tensor(x_in)
+                y_out = tf.constant([x_center, y_center, confidence])
+                yield (x_in, y_out)
+
+        def __new__(cls):
+            output_shape = (im_height,im_width,im_channels)
+            return tf.data.Dataset.from_generator(
+                cls._generator,
+                output_signature=(
+                    tf.TensorSpec(shape=output_shape, dtype=tf.float32),
+                    tf.TensorSpec(shape=(3,), dtype=tf.float32)
+                )
+            )
+    
+    class DatasetAugmentation:
+        def __call__(self, image, label, training=True):
+            from tensorflow.keras.layers.experimental import preprocessing
+            data_augmentation = tf.keras.Sequential([
+                preprocessing.RandomContrast(0.3),
+            ])
+            image = data_augmentation(image, training=training)
+            return image, label
+
     def create_model():
         from model import SoccerBotModel
         module = SoccerBotModel()
@@ -77,45 +98,68 @@ if __name__ == "__main__":
         y_out = module(x_in)
         model = tf.keras.Model(inputs=x_in, outputs=y_out)
         return model
-    
-    def calculate_metrics(Y_pred, Y_expected):
-        # shape = B,3 => x_center, y_center, confidence
+   
+    def detect_accuracy(y_true, y_pred, thresh=0.8):
+        true_cls = y_true[:,2]
+        pred_cls = y_pred[:,2]
+        pred_cls = tf.math.greater(pred_cls, thresh)
+        pred_cls = tf.cast(pred_cls, true_cls.dtype)
         
-        # accuracy of confidence score
-        threshold = 0.5
-        pred_confidence = tf.cast(tf.math.greater(Y_pred[:,2], threshold), tf.float32)
-        expected_confidence = Y_expected[:,2]
-        confidence_accuracy = 1.0-tf.math.reduce_mean(tf.math.abs(pred_confidence - expected_confidence))
+        abs_err = tf.math.abs(true_cls-pred_cls)
+        return 1-tf.math.reduce_mean(abs_err)
 
-        # error of confidence score
-        mean_confidence_error = tf.math.reduce_mean(tf.math.abs(Y_pred[:,2] - Y_expected[:,2]))
-
-        # bounding box error when there is a ball
-        pred_position = Y_pred[:,:2]
-        expected_position = Y_expected[:,:2]
-        position_error = tf.math.reduce_sum(tf.math.abs(pred_position-expected_position), axis=1)
-        mean_position_error = tf.math.reduce_mean(tf.math.multiply(position_error, expected_confidence))
-
-        # anomalous bounding box when there isn't a ball 
-        mean_position_anomaly = tf.math.reduce_mean(tf.math.multiply(position_error, 1-expected_confidence))
+    def position_accuracy(y_true, y_pred):
+        true_cls = y_true[:,2]
+        true_pos = y_true[:,:2]
+        pred_pos = y_pred[:,:2]
         
-        return tf.stack([confidence_accuracy, mean_confidence_error, mean_position_error, mean_position_anomaly])
+        dist_sqr_err = tf.math.square(true_pos-pred_pos)
+        dist_sqr_err = tf.math.reduce_sum(dist_sqr_err, axis=1)
+        dist_err = tf.math.sqrt(dist_sqr_err)
+        
+        # only consider when object is there
+        dist_err = tf.math.multiply(dist_err, true_cls)
+        net_err = tf.math.reduce_sum(dist_err)
+        total_objects = tf.math.reduce_sum(true_cls)
+        
+        mean_err = net_err / total_objects
+        return 1-mean_err
+
+    def custom_loss_fn(y_true, y_pred):
+        true_cls = y_true[:,2]
+        pred_cls = y_pred[:,2]
+        true_pos = y_true[:,:2]
+        pred_pos = y_pred[:,:2]
+
+        bce = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        cat_err = bce(true_cls, pred_cls)
+        
+        # NOTE: use L1 distance instead of L2, this works better
+        pos_err = tf.math.abs(true_pos-pred_pos)
+        pos_err = tf.math.reduce_sum(pos_err, axis=1)
+        pos_err = tf.math.reduce_mean(pos_err)
+        
+        W0 = 4.0
+        net_err = cat_err + W0*pos_err
+        return net_err
+        
     
     model = create_model()
-    model.summary()
+    if args.print_model_summary:
+        model.summary()
 
     # training setup
     hyperparams = {
         "init_lr": 1e-3, 
-        "total_train_batches": 20,
-        "total_test_batches": 2,
-        "train_batch_size": 64,
-        "test_batch_size": 64,
-        "epochs": args.epochs,
     }
 
     optimizer = tf.keras.optimizers.Adam(learning_rate=hyperparams["init_lr"])
-    loss_fn = tf.keras.losses.MeanAbsoluteError()
+
+    model.compile(
+        loss=custom_loss_fn, 
+        optimizer=optimizer,
+        metrics=[detect_accuracy, position_accuracy]
+    )
 
     try:
         model.load_weights(args.model_in)
@@ -123,45 +167,29 @@ if __name__ == "__main__":
     except Exception as ex:
         print(f"Failed to load in weights from '{args.model_in}': {ex}")
     
-    print(f"Start of training")
-
-    async_train_batch_generator = AsyncBatchGenerator(lambda: create_batch(hyperparams["train_batch_size"]), max_batches=2)
-    async_test_batch_generator = AsyncBatchGenerator(lambda: create_batch(hyperparams["test_batch_size"]), max_batches=2)
-    try:
-        curr_epoch = 0
-        # NOTE: Run using directml
-        with tf.device(f"/device:{args.device}"):
-            for curr_epoch in range(hyperparams["epochs"]):
-                train_loss_avg = tf.keras.metrics.Mean()
-                for _ in range(hyperparams["total_train_batches"]):
-                    X_in, Y_out = async_train_batch_generator.get_batch()
-                    with tf.GradientTape() as tape:
-                        Y_pred = model(X_in, training=True)
-                        loss = loss_fn(Y_out, Y_pred)
-                        grad = tape.gradient(loss, model.trainable_variables)
-                    optimizer.apply_gradients(zip(grad, model.trainable_variables))
-                    train_loss_avg.update_state(loss)
-            
-                test_loss_avg = tf.keras.metrics.Mean()
-                test_metrics_avg = tf.keras.metrics.MeanTensor()
-                for i in range(hyperparams["total_test_batches"]):
-                    X_in, Y_out = async_test_batch_generator.get_batch()
-                    Y_pred = model(X_in)
-                    loss = loss_fn(Y_out, Y_pred)        
-                    metrics = calculate_metrics(Y_pred, Y_out)
-            
-                    test_loss_avg.update_state(loss)
-                    test_metrics_avg.update_state(metrics)
-                    
-                confidence_accuracy, mean_confidence_error, mean_position_error, mean_position_anomaly = test_metrics_avg.result()
-                print(f"epoch={curr_epoch} | train_loss={train_loss_avg.result():.2e} test_loss={test_loss_avg.result():.2e} | " +
-                    f"conf_acc={confidence_accuracy:.2e} conf_err={mean_confidence_error:.2e} | " + 
-                    f"pos_err={mean_position_error:.2e} pos_anom={mean_position_anomaly:.2e}")
-    except KeyboardInterrupt:
-        print(f"Interrupted at epoch={curr_epoch}")
+    ds_augment = DatasetAugmentation()
+    dataset = tf.data.Dataset.range(TOTAL_DATA_THREADS)
+    dataset = dataset.interleave(
+        lambda _: ArtificialDataset(),
+        num_parallel_calls=tf.data.AUTOTUNE
+    )
+    dataset = dataset.batch(args.batch_size)
+    dataset= dataset.map(ds_augment, num_parallel_calls=tf.data.AUTOTUNE)
     
-    model.save_weights(args.model_out)
-    print(f"Saved weights to '{args.model_out}'")
-    exit(0)
+    print(f"Start of training")
+    is_save_weights = True
+    try:
+        with tf.device(f"/device:{args.device}"):
+            model.fit(dataset, steps_per_epoch=args.steps_per_epoch, epochs=args.epochs)
+    except KeyboardInterrupt:
+        print(f"Interrupted training early")
+        response = input("Do you want to save weights? [Y/N]: ")
+        response = response.lower().strip()
+        if not (response == "y" or response == "yes"):
+            is_save_weights = False
+
+    if is_save_weights: 
+        model.save_weights(args.model_out)
+        print(f"Saved weights to '{args.model_out}'")
 
     
