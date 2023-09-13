e3fca46531b3e84a0bd7156da508408bf1865137
William Yang <williamyang2017@gmail.com>
2023-09-13 14:39:06 +1000

    Fixed use of image before loading from file

--- scripts/training/generator/config.py
+++ scripts/training/generator/config.py
@@ -8,20 +8,25 @@ class GeneratorConfig:
         self.score_font = None 
     
     def set_ball_image(self, filepath):
-        self.ball_image = Image.open(filepath)
+        self.ball_image = self._load_image(filepath)
     
     def set_emote_images(self, filepaths):
         images = []
         for filepath in filepaths:
-            image = Image.open(filepath)
+            image = self._load_image(filepath)
             images.append(image)
         self.emote_images = images
     
     def set_background_image(self, filepath):
-        self.background_image = Image.open(filepath)
+        self.background_image = self._load_image(filepath)
     
     def set_score_font(self, font_name, size):
         self.score_font = ImageFont.truetype(font_name, size)
+
+    def _load_image(self, filepath):
+        image = Image.open(filepath)
+        image.load()
+        return image
     
     def assert_valid(self):
         assert(self.ball_image != None)
