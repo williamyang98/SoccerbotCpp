3206ebd7d2fa5b0f1e1ee54af55734b39ffd2012
William Yang <williamyang2017@gmail.com>
2023-09-24 01:05:40 +1000

    Removed vcpkg from cmake configure script

--- cmake_configure.sh
+++ cmake_configure.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-if [[ -z "$VCPKG_ROOT" ]]; then
-    TOOLCHAIN_FILE="C:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake"
-else
-    TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
-fi
-
 if [[ -z "$BUILD_OUT" ]]; then
     BUILD_OUT="build"
 fi
@@ -17,5 +11,4 @@ cmake .\
  -B $BUILD_OUT\
  -G Ninja\
  -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE\
- -DCMAKE_TOOLCHAIN_FILE:STRING=$TOOLCHAIN_FILE\
  -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE
